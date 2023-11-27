7d3fca333cbde65f8310b1b4790a5d9ada5b6adf
MikeBlokhin <mls39rus@gmail.com>
2023-11-27 02:43:06 +0200

    Solved

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+L
+src/main/java/AppConfig.java,e\8\e8c6e8d3ed0c1a112bf172749a32016efd933dff
+M
+src/main/java/HelloWorld.java,0\e\0ea593bf68cb2752d12ea9f4a9be88eed88a4424
+F
+src/main/java/App.java,6\7\67512e2f04487cca15d6699055f74a51de1a9eb4
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,33 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+/**Задание:
 
+ Прочитайте про виды бинов.
+V Создайте POJO-класс Cat.
+V В классе AppConfig, по аналогии, создайте бин с именем “cat”.
+V Настройте этот бин так, чтобы он создавался новым при каждом запросе.
+V В классе App, по аналогии, вызовите еще раз бин HelloWorld,
+ затем 2 раза вызовите бин cat. Сравните 2 пары бинов по ссылке и
+ выведите результат в консоль. Для пары HelloWorld должно вернуться
+ true, для cat - false. Так вы можете увидеть результат того,
+ как работает
+ наш контейнер.**/
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld otherBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat otherCat = applicationContext.getBean(Cat.class);
+
+        System.out.println(bean == otherBean);
+        System.out.println(cat == otherCat);
+
+
     }
 }
